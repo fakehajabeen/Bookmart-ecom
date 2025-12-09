345eeb336a0055d4c3288e2d36fdacde326e0945
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-12-09 21:11:47 +0530

    Fix import path for user sign-in controller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 const userSignUpController = require("../controller/user/userSignup")
-const userSignInController = require('../controller/user/userSignIn')
+const userSignInController = require('../controller/user/userSignin')
 const userDetailsController = require('../controller/user/userDetails')
 const authToken = require('../middleware/authToken')
 const userLogout = require('../controller/user/userLogout')
